f81e5bb5c5e8f6a241ac8da14e975daa3f9bf5c8
Ali Aman Khan <aliamankhan96@gmail.com>
2026-01-31 23:16:07 +0530

    API-Gateway completed

---
+++ Backend/API-Gateway/src/config/services.js
@@ -0,0 +1,6 @@
+module.exports = {
+  USER_SERVICE: "http://localhost:8080",
+  PITCH_SERVICE: "http://localhost:8081",
+  COLLAB_SERVICE: "http://localhost:8083",
+  MESSAGE_SERVICE: "http://localhost:8084",
+};
