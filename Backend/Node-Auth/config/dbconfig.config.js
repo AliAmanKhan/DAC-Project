cf80a88e0e3145d0a61f23992a681ddaa7856fab
Ali Aman Khan <aliamankhan96@gmail.com>
2026-01-31 23:16:47 +0530

    Auth Service Completed

--- Backend/Node-Auth/config/dbconfig.config.js
+++ Backend/Node-Auth/config/dbconfig.config.js
@@ -4,7 +4,7 @@ const connection = db.createConnection({
     host: "localhost",
     user: "root",
     password: "root123",
-    database: "collabit"
+    database: "collabit_user"
 })
 
 connection.connect((err) => {
