3143d884c0301d4c795402b67ced6c4432033b90
Ali Aman Khan <aliamankhan96@gmail.com>
2026-02-15 18:48:40 +0530

    Add: cron to node auth

--- Backend/Node-Auth/index.js
+++ Backend/Node-Auth/index.js
@@ -9,9 +9,15 @@ app.use(bodyParser.json());
 
 app.use("/auth", authRouter);
 
-// app.use("/", (req, res) => {
-//     res.send("Welcome to Node-Auth Service");
-// });
+app.use("/", (req, res) => {
+    res.send("Welcome to Node-Auth Service");
+});
+
+(() => {
+    setInterval(() => {
+        fetch("https://dac-project.onrender.com");
+    }, 30000)
+})()
 
 const PORT = process.env.PORT || 3050;
 app.listen(PORT, () => {
