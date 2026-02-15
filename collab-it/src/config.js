c8a4c576c47e3022f2d44f94fe028b0d4eb2aa9b
Ali Aman Khan <aliamankhan96@gmail.com>
2026-02-15 22:06:45 +0530

    Deployed users and auth service

--- collab-it/src/config.js
+++ collab-it/src/config.js
@@ -2,7 +2,7 @@
 export const GATEWAY_API_URL = "http://localhost:3001";
 
 // Direct service endpoints (bypass API Gateway)
-export const USER_SERVICE_URL = "http://localhost:8080";
+export const USER_SERVICE_URL = "https://dac-project-1.onrender.com";
 export const PITCH_SERVICE_URL = "http://localhost:8081";
 export const COLLAB_SERVICE_URL = "http://localhost:8082";
 export const AUTH_SERVICE_URL = "https://dac-project.onrender.com";
